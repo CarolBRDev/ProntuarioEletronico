4377b452a04e993721dd5b0ab097bbe5665abf0e
CarolBRDev <carolbeatrice@gmail.com>
2024-10-29 21:15:59 -0300

    commit inicial

---
+++ ProntuarioEletronico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProntuarioEletronico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
